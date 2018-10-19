758026bcdfb9ec7207aae87a2f542d8d8f54ad04
jrkerr2 <johnrkerr@hotmail.com>
2018-10-19 16:17:44 -0400

    updated entry point in package.json

--- server.js
+++ server.js
@@ -11,22 +11,6 @@ var app = express();
 app.engine('handlebars', exphbs({defaulLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-
-
-app.get('/website', function(req, res){
-  request('https://www.quora.com/profile/Pamela-Salon-Syntactics-Inc', function(error, response, body){
-    // res.send(body)
-    var $ = cheerio.load(body)
-    var array = []
-    $('.story_title_container').each(function(){
-      var url = $(this).children('div').children('div').children('a').attr('href')
-      var title = $(this).children('div').children('div').children('a').children('span').children('span').text()
-      array.push({title: title, url: url})
-    })
-    res.send(array)
-  })
-})
-
 // Database configuration
 // Save the URL of our database as well as the name of our collection
 var databaseUrl = "root:root@192.168.99.100/john_test?authSource=admin";
@@ -40,9 +24,26 @@ db.on("error", function(error) {
   console.log("Database JOHN_TEST Error:", error);
 });
 
-//routes
+
+
+app.get('/website', function(req, res){
+  request('https://www.quora.com/profile/Pamela-Salon-Syntactics-Inc', function(error, response, body){
+    // res.send(body)
+    var $ = cheerio.load(body)
+    var urlArray = []
+    $('.story_title_container').each(function(){
+      var url = $(this).children('div').children('div').children('a').attr('href')
+      var title = $(this).children('div').children('div').children('a').children('span').children('span').text()
+      array.push({title: title, url: url})
+    })
+    res.send(urlArray);
+  })
+});
+
+
+//GET routes
 app.get('/', function(req,res){
-  res.send("JOHN test! 4");
+  res.send("JOHN test! 5");
 
 });
 
@@ -62,8 +63,38 @@ app.get("/all", function(req, res) {
 
 app.get('/data', function(req,res){
   res.json(data)
-})
+});
 
+// POST routes
+app.post("/addExample", function(req, res) {
+  // Query: In our database, go to the animals collection, then "find" everything
+  db.testCollect.find({}, function(error, found) {
+    // Log any errors if the server encounters one
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise, send the result of this query to the browser
+    else {
+      res.json(found);
+    }
+  });
+});
+
+app.post('/add', (req, res, next) => {
+
+  var name = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name
+  };
+
+  dbase.collection("name").save(name, (err, result) => {
+    if(err) {
+      console.log(err);
+    }
+
+    res.send('name added successfully');
+  });
+});
 
 // Set the app to listen on port 3000
 app.listen(process.env.PORT || 3000, function() {
