e69c65d383b1838f25c06ef85efa8234fb3756d4
jrkerr2 <johnrkerr@hotmail.com>
2018-10-20 08:46:36 -0400

    scraping progress

--- server.js
+++ server.js
@@ -41,13 +41,14 @@ mongoose.connect(dbCon, { useNewUrlParser: true }, function(error) {
 
 
 app.get('/website', function(req, res){
-  request('https://www.prnewswire.com/news-releases/news-releases-list/', function(error, response, body){
+  request('https://text.npr.org/', function(error, response, body){
     // res.send(body)
     var $ = cheerio.load(body);
     var urlObj = {};
     $('.story_title_container').each(function(){
       var url = $(this).children('div').children('div').children('a').attr('href')
       var title = $(this).children('div').children('div').children('a').children('span').children('span').text()
+      
       array.push({title: title, url: url})
     })
     res.send(urlObj);
